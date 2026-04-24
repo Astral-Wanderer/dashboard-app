56910ea883073dbd91a67e1879d15068a0fc212a
Marah Mohammed <marahmohammed327@.gmail.com>
2026-04-24 17:56:08 +0200

    Improve readability of ProjectsPage.tsx and ProjectDetailPage.tsx

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -6,9 +6,21 @@ export default function ProjectsPage() {
     return (
         <div>
             <h1>Projects</h1>
+
             {mockProjects.map((p: Project) => (
-                <Link to={`/projects/${p.id}`} style={{ textDecoration: 'none' }} key={p.id}>
-                    <div style={{background:'#fff', borderRadius:8, padding:16, marginBottom:12 }}>
+                <Link 
+                to={`/projects/${p.id}`} 
+                style={{ textDecoration: 'none' }} 
+                key={p.id}
+                >
+                    <div 
+                        style={{
+                            background: '#fff', 
+                            borderRadius:8, 
+                            padding: 16, 
+                            marginBottom: 12 
+                        }}
+                    >
                         <h3>{p.name}</h3>
                         <p style={{ color:'#64748B' }}>{p.description}</p>
                     </div>
