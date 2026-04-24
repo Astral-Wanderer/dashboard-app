38063919015f9ea7e3fb6c79877a455f209fe75a
Marah Mohammed <marahmohammed327@.gmail.com>
2026-04-24 17:26:39 +0200

    Day 5 - Step 1: Add navigation from ProjectsPage to detail

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,15 +1,18 @@
 import { mockProjects } from '../utils/mockData';
 import type { Project } from '../types';
+import { Link } from 'react-router-dom';
 
 export default function ProjectsPage() {
     return (
         <div>
             <h1>Projects</h1>
             {mockProjects.map((p: Project) => (
-                <div key={p.id} style={{background:'#fff', borderRadius:8, padding:16, marginBottom:12 }}>
-                    <h3>{p.name}</h3>
-                    <p style={{ color:'#64748B' }}>{p.description}</p>
-                </div>
+                <Link to={`/projects/${p.id}`} style={{ textDecoration: 'none' }} key={p.id}>
+                    <div style={{background:'#fff', borderRadius:8, padding:16, marginBottom:12 }}>
+                        <h3>{p.name}</h3>
+                        <p style={{ color:'#64748B' }}>{p.description}</p>
+                    </div>
+                </Link>
             ))}
         </div>
     );
